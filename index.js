9477fec80724476164254e622643963005b39bc5
rouenemedina <rouenemedina@gmail.com>
2024-10-10 16:40:21 -0400

    created directories for controllers and routes

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
+app.get ("/", (req, res) => {
+    rs.send("Server is running");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
