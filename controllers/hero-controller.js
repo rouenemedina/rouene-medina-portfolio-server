66105c072913e97517fc089a4d496c459728557a
rouenemedina <rouenemedina@gmail.com>
2024-10-14 21:41:29 -0400

    created the hero-controller

--- controllers/hero-controller.js
+++ controllers/hero-controller.js
@@ -1,3 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import fs from "fs";
+
+//function to read the hero.json file 
+const readHero = () => {
+    const heroFile = fs.readFileSync("./data/hero.json");
+    const heroData = JSON.parse(heroFile);
+    return heroData;
+}
+
+// GET /hero
+app.get("/hero", (req, res) => {
+    const heroData = readHero();
+})
