71b94f72d00ff1e119c8a8374cdf30f2b1f243a2
rouenemedina <rouenemedina@gmail.com>
2024-10-22 18:46:20 -0400

    installed postgres and knex for database

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+
+export default {
+    client: "pg",
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        charset: "utf8"
+    }
+}
