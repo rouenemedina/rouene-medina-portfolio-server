d22f71763add0baa7a2b9d893b9c6927e1305f49
rouenemedina <rouenemedina@gmail.com>
2024-10-31 01:40:44 -0400

    removed unused/commented code

--- controllers/hero-controller.js
+++ controllers/hero-controller.js
@@ -4,33 +4,6 @@ import configuration from "../knexfile.js";
 import "dotenv/config";
 
 const knex = initKnex(configuration);
-
-//function to read the hero.json file 
-// const readHero = () => {
-//     const heroFile = fs.readFileSync("./data/hero.json");
-//     const heroData = JSON.parse(heroFile);
-//     return heroData;
-// }
-
-// GET /hero
-// get hero data from json file
-// const getHero = async (req, res) => {
-//     const heroData = readHero();
-
-//     const rawData = heroData.map((hero) => {
-//         return {
-//             id: hero.id,
-//             name: hero.name,
-//             position: hero.position,
-//             location: hero.location,
-//             image: hero.image
-//         }
-//     })
-
-//     res.json(rawData);
-// }
-
-// get hero data from database
 const getHeroData = async (req, res) => {
     try {
         const response = await knex("hero").select();
