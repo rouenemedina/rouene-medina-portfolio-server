3edcda36bceeac1ec3d692029986b0e3d9adbd51
rouenemedina <rouenemedina@gmail.com>
2024-11-07 02:14:13 -0500

    removed unused codes

--- controllers/hero-controller.js
+++ controllers/hero-controller.js
@@ -5,29 +5,13 @@ import "dotenv/config";
 import { getImageUrl } from "../helpers/cloudinary-helper.js";
 
 const knex = initKnex(configuration);
-
-const getHeroImageUrl = async (req, res) => {
-    const { public_id } = req.params;
-    try {
-        const imageUrl = await getImageUrl(public_id, "hero");
-        res.status(200).json({
-            message: "Image url has been retrieved.",
-            data: imageUrl
-        });
-    } catch(err) {
-        console.log("Error fetching data", err);
-        res.status(400).json({
-            message: "Error retrieving data.", 
-            err: "400"
-        });
-    }
-}
 const getHeroData = async (req, res) => {
     try {
         const response = await knex("hero").select();
 
-        const heroImageUrl = await getHeroImageUrl(req, res);
-        res.status(200).json(response);
+        const imageUrl = await getImageUrl(public_id, "hero");
+
+        res.status(200).json(response, imageUrl);
     } catch (err) {
         console.log("Error fetching data", err);
         res.status(400).json({
