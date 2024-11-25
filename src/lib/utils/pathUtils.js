b2b9481ab7512d19b2c0506b223750a4ed421e55
rouenemedina <rouenemedina@gmail.com>
2024-11-24 23:20:20 -0500

    troubleshooting paths

--- src/lib/utils/pathUtils.js
+++ src/lib/utils/pathUtils.js
@@ -1,7 +1,8 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export { __filename, __dirname };
