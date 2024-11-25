5f278e559094a7d19216f847e592ffbb0f43714d
rouenemedina <rouenemedina@gmail.com>
2024-11-24 23:26:32 -0500

    troubleshooting paths

--- src/controllers/landing-controller.js
+++ src/controllers/landing-controller.js
@@ -6,7 +6,7 @@ import { __filename } from "../lib/utils/pathUtils.js";
 // Function to read JSON files
 const readLandingFile = async () => {
   try {
-    const filePath = path.join(__dirname, "../../data/landing.json");
+    const filePath = path.join(__dirname, "../data/landing.json");
     console.log("Computed file path:", filePath);
 
     // Check if file exists asynchronously
